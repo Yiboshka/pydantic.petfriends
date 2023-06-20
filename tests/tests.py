1579c898af37fc89cc3f17d191721196f1587b60
Татьяна Коробовцева <124469415+Yiboshka@users.noreply.github.com>
2023-06-21 00:16:40 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,6 @@
-from api import PetFriends
-from pydantic import BaseModel, Field
 import pytest
-
-class apikey(BaseModel):
-    key: str = Field()
-
-class pets_param(BaseModel):
-    age: str = Field()
-    animal_type: str = Field()
-    created_at: str = Field()
-    id: str = Field()
-    name: str = Field()
-    pet_photo: str = Field()
-    user_id: str = Field(None)
+from api import PetFriends
+from serialize import apikey, pets_param
 
 pf = PetFriends()
 
